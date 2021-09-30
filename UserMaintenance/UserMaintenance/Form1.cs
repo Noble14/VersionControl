f349472a4d9fe92ce68f58c70172c002bc6ddfa6
Zoltán Nemes <zoltan.nemes@stud.uni-corvinus.hu>
2021-09-30 13:24:24 +0200

    add funkció implementáció users lista

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,17 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
+
             label1.Text = Resource1.LastName;
             label2.Text = Resource1.FirstName;
             button1.Text = Resource1.Add;
+
+            listBox1.DataSource = users;
+            listBox1.DisplayMember = "FullName";
+            listBox1.ValueMember = "ID";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var a = new User();
+            a.FirstName = label2.Text;
+            a.LastName = label1.Text;
+            users.Add(a);
         }
     }
 }
