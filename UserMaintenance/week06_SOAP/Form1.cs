a194347e849f425a13a2a92c1957545a8ee145bf
Zoltán Nemes <zoltan.nemes@stud.uni-corvinus.hu>
2021-10-28 12:50:00 +0200

    3 Webszolgáltatás hívása

--- UserMaintenance/week06_SOAP/Form1.cs
+++ UserMaintenance/week06_SOAP/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week06_SOAP.MnbServiceReference;
+using System.IO;
 
 namespace week06_SOAP
 {
@@ -15,6 +17,31 @@ namespace week06_SOAP
         public Form1()
         {
             InitializeComponent();
+            method();
+        }
+
+        private void method()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
+            request.currencyNames = "EUR";
+            request.startDate = "2020-01-01";
+            request.endDate = "2020-06-30";
+            var response = mnbService.GetExchangeRates(request);
+            string result = response.GetExchangeRatesResult;
+            //SaveResult(result);
+        }
+
+        private void SaveResult(string res)
+        {
+            SaveFileDialog svf = new SaveFileDialog();
+            if (svf.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(svf.FileName))
+                {
+                    sw.WriteLine(res);
+                }
+            }
         }
     }
 }
