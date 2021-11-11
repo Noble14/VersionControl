9fb2a7ff4b93c83edbb95e46d3b2c374efa5f7f0
Zoltán Nemes <zoltan.nemes@stud.uni-corvinus.hu>
2021-11-11 12:31:06 +0100

    Modification of View

--- UserMaintenance/week08_factory/Form1.cs
+++ UserMaintenance/week08_factory/Form1.cs
@@ -4,22 +4,21 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using week08_factory.Entities;
+using week08_factory.Abstractions;
 
 namespace week08_factory
 {
     public partial class Form1 : Form
     {
         #region Fields
-        private List<Ball> _balls = new List<Ball>();
-        private BallFactory _factory;
+        private List<Toy> _toys = new List<Toy>();
+        private IToyFactory _factory;
         #endregion
 
         #region Properties
-        public BallFactory Factory
+        public IToyFactory Factory
         {
             get { return _factory; }
             set { _factory = value; }
@@ -30,7 +29,7 @@ namespace week08_factory
         public Form1()
         {
             InitializeComponent();
-            _factory = new BallFactory();
+            _factory = new CarFactory();
         }
         #endregion
 
@@ -38,9 +37,9 @@ namespace week08_factory
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             int pos = 0;
-            foreach (Ball b in _balls)
+            foreach (Toy b in _toys)
             {
-                b.MoveBall();
+                b.MoveToy();
                 if (b.Left > pos)
                 {
                     pos = b.Left;
@@ -48,16 +47,16 @@ namespace week08_factory
             }
             if (pos >= 1000)
             {
-                Ball toDelete = _balls[0];
-                _balls.Remove(toDelete);
+                Toy toDelete = _toys[0];
+                _toys.Remove(toDelete);
                 mainPanel.Controls.Remove(toDelete);
             }
         }
         private void createTimer_Tick(object sender, EventArgs e)
         {
-            Ball b = Factory.CreateNew();
+            Toy b = Factory.CreateNew();
             b.Left = -b.Width;
-            _balls.Add(b);
+            _toys.Add(b);
             mainPanel.Controls.Add(b);
         }
         #endregion
