ab1e8db01ac8baea1f682bbfb41ebbaf39512bdc
Zoltán Nemes <zoltan.nemes@stud.uni-corvinus.hu>
2021-11-11 11:17:27 +0100

    BallFActory_CreateNew

---
+++ UserMaintenance/week08_factory/Entities/BallFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week08_factory.Entities
+{
+    class BallFactory
+    {
+        #region Public methods
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+        #endregion
+    }
+}
