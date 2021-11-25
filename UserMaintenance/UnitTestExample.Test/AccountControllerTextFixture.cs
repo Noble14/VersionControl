e36ea11a5f517baa81748c261df399f8756844ba
Zoltán Nemes <p4pc4k@INF.ELTE.HU>
2021-11-25 10:44:08 +0100

    email validation function

--- UserMaintenance/UnitTestExample.Test/AccountControllerTextFixture.cs
+++ UserMaintenance/UnitTestExample.Test/AccountControllerTextFixture.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 
 namespace UnitTestExample.Test
@@ -13,7 +14,14 @@ namespace UnitTestExample.Test
         [Test]
         private void TestValidateEmail(string email, bool expectedResult)
         {
+            //Arrange
+            AccountController ac = new AccountController();
 
+            //Act
+            bool l = ac.ValidateEmail(email);
+
+            //Assert
+            Assert.AreEqual(expectedResult, l);
         }
     }
 }
