8d8257f8ef34a6e4e5f7238cce5666976cbe9d52
Zoltán Nemes <p4pc4k@INF.ELTE.HU>
2021-11-25 11:00:10 +0100

    TestPasswordValidation

--- UserMaintenance/UnitTestExample.Test/AccountControllerTextFixture.cs
+++ UserMaintenance/UnitTestExample.Test/AccountControllerTextFixture.cs
@@ -29,5 +29,24 @@ namespace UnitTestExample.Test
             //Assert
             Assert.AreEqual(expectedResult, l);
         }
+        [
+            Test,
+            TestCase("AzEnKutyamNeveSzamNelkul", false),
+            TestCase("CSUPANAGYBETU2", false),
+            TestCase("csupakisbetuvanitt2", false),
+            TestCase("Rovid22", false),
+            TestCase("PerfectPassword4", true)
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            //Arrange
+            AccountController ac = new AccountController();
+
+            //Act
+            bool actualResult = ac.ValidatePassword(password);
+
+            //Assert
+            Assert.AreEqual(expectedResult,actualResult);
+        }
     }
 }
