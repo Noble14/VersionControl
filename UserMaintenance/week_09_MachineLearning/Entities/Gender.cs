c7deb273c29ee9ffc4f015dcc9bcaceea6fbeaf6
Zoltán Nemes <p4pc4k@INF.ELTE.HU>
2021-12-02 15:23:15 +0100

    Osztáyok létrehozása

---
+++ UserMaintenance/week_09_MachineLearning/Entities/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_09_MachineLearning.Entities
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+}
---
+++ UserMaintenance/week_09_MachineLearning/Entities/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_09_MachineLearning.Entities
+{
+    public class Person
+    {
+        public Gender Gender { get; set; }
+        public int Birthday { get; set; }
+        public int NumberOfChildren { get; set; }
+        public bool IsAlive { get; set; }
+
+        public Person()
+        {
+            IsAlive = true;
+        }
+    }
+}
